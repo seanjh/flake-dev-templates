1f33165934c82fd704211a92dcf58b370331e832
Sean Herman <sherman1@rbi.com>
2025-01-30 21:59:53 -0500

    fixed

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
     {
       templates = {
         ruby = {
-          path = ./templates/ruby;
+          path = ./ruby;
           description = "Ruby development environment";
         };
         ruby-on-rails = {
-          path = ./templates/ruby-on-rails;
+          path = ./ruby-on-rails;
           description = "Ruby On Rails development environment";
         };
       };
